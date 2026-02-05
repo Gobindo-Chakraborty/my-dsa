fd3e9b4c1aeaa9c12b91d5a0384d867134376fc9
Gobindo Chakraborty <gobindochakraborty108@gmail.com>
2026-02-05 18:42:40 +0600

    Academind added

---
+++ academind/index.js
@@ -0,0 +1,13 @@
+const arr = [2, 5, 5];
+const sets = new Set();
+const maps = new Map();
+
+sets.add("hello");
+sets.add(34);
+
+maps.set("First-name", "John");
+maps.set("Age", 34);
+
+console.log(arr);
+console.log(sets);
+console.log(maps);
